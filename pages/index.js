041ece30a1fb9695eaed0e3254e422c53f3aee76
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-27 04:41:49 +0530

    add bubbles animation

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,18 @@ export default function Home() {
       </Head>
 
       <main className="rounded-md">
+        <div id="background-wrap">
+          <div class="bubble x1"></div>
+          <div class="bubble x2"></div>
+          <div class="bubble x3"></div>
+          <div class="bubble x4"></div>
+          <div class="bubble x5"></div>
+          <div class="bubble x6"></div>
+          <div class="bubble x7"></div>
+          <div class="bubble x8"></div>
+          <div class="bubble x9"></div>
+          <div class="bubble x10"></div>
+        </div>
         <div className="flex relative pl-10">
           <BrandBox />
 
