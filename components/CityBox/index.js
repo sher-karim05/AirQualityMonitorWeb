e9dbee4003578adc8979e1cebc393b1c31720fb7
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-27 16:59:44 +0530

    add graph action button on CB

--- components/CityBox/index.js
+++ components/CityBox/index.js
@@ -1,15 +1,17 @@
-import clsx from "clsx";
 import React from "react";
 import ReactTimeAgo from "react-time-ago";
 import CardContainer from "../../containers/CardContainer";
 import { getAQIStandard } from "../../utils";
+import Image from "next/image";
+
+// import imgSpark from "/assets/images/spark.png";
 
 function CityBox({ title, aqi, date }) {
   const [aqiBase, aqiDecimal] = (aqi + "").split(".");
 
   return (
-    <div className="lg:w-1/2 xl:w-1/3 p-5">
-      <CardContainer className="w-full bg-white border-2 border-primary-light">
+    <div className="lg:w-1/2 xl:w-1/3 p-5 relative">
+      <CardContainer className="w-full bg-white border-2 border-primary-light pb-8 hover:shadow-lg transition-all">
         <div className="flex justify-between">
           <div>
             <div className="text-xl">{title}</div>
@@ -28,6 +30,13 @@ function CityBox({ title, aqi, date }) {
           </div>
         </div>
       </CardContainer>
+
+      <div
+        className="rounded-full bg-white w-10 h-10 absolute bottom-0 ml-5 border-2 border-primary-light overflow-hidden p-2 cursor-pointer transform hover:scale-110 transition-all"
+        title="View Graph"
+      >
+        <Image src="/assets/images/graph.png" width="100" height="100" />
+      </div>
     </div>
   );
 }
