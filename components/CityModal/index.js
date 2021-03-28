e671808955f2985e4b32d185b10295545cc15f5e
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-28 06:02:47 +0530

    show styled city info on CityModal

--- components/CityModal/index.js
+++ components/CityModal/index.js
@@ -9,17 +9,20 @@ function CityModal({ visible, hide, city = {} }) {
 
   return (
     <Modal visible={visible} width="90%" height="80%" onClickAway={hide}>
-      <div>
-        <div className="text-2xl">{city.city}</div>
-        <div className="text-md">
-          <ReactTimeAgo
-            date={city.date || new Date().getTime()}
-            locale="en-US"
-          />
-        </div>
+      <div className="p-5">
         <div className={"text-" + getAQIStandard(city.aqi).bgColor}>
-          <span className="text-4xl">{aqiBase}</span>
-          <span className="text-md">.{aqiDecimal}</span>
+          <span className="text-5xl">{aqiBase}</span>
+          <span className="text-xl">.{aqiDecimal}</span>
+        </div>
+
+        <div className="mt-2">
+          <span className="text-2xl">{city.city}</span>
+          <span className="text-xs text-gray-600 ml-2">
+            <ReactTimeAgo
+              date={city.date || new Date().getTime()}
+              locale="en-US"
+            />
+          </span>
         </div>
       </div>
     </Modal>
